18ede66d652f6765081fecc8da2c4c994ae0ffb2
MasterYoda12 <maxime.ladu33@gmail.com>
2025-01-07 17:10:32 +0100

    de

--- panier.php
+++ panier.php
@@ -2,7 +2,6 @@
 session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     $numero = $_POST['numero'];
-    // Add the book to the cart (this is just a placeholder, implement your cart logic here)
     echo "Livre ajouté au panier avec succès!";
 } else {
     echo "Vous devez être connecté pour ajouter un livre au panier.";
