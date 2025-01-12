59b9d795822429ba906d4fe0d4ca443c752fc2df
MasterYoda12 <maxime.ladu333@gmail.com>
2025-01-12 16:43:54 +0100

    TERMINER

--- panier.php
+++ panier.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <title>Titre de la page</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="styles.css"/>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</head>
-<body>
 <div class="container">
   <div class="row">
     <!-- Contenu principal -->
@@ -21,28 +10,34 @@
       <?php include 'entete.php';?> 
       <h2 class="text-success text-center my-4">Votre Panier</h2>
       <?php
-      require_once('connexion.php');
+      
+      require_once('connexion.php'); 
 
-      if (isset($_POST['numero'])) {
-          $numero = $_POST['numero'];
 
-          $stmt = $connexion->prepare("SELECT prenom, nom, l.titre, l.anneeparution, l.isbn13, l.detail, l.photo FROM auteur a INNER JOIN livre l ON a.noauteur = l.noauteur WHERE nolivre = :numero");
-          $stmt->bindValue(":numero", $numero);
+      if (isset($_POST['numero'])) { // vérifie si le numéro est envoyé via un POST 
+          $numero = $_POST['numero']; // récupère le numéro du livre envoyé
+
+          $stmt = $connexion->prepare("SELECT prenom, nom, l.titre, l.anneeparution, l.isbn13, l.detail, l.photo, l.nolivre FROM auteur a INNER JOIN livre l ON a.noauteur = l.noauteur WHERE nolivre = :numero");
+          $stmt->bindValue(":numero", $numero); // lie le numéro du livre à la requête SQL
           $stmt->setFetchMode(PDO::FETCH_OBJ);
           $stmt->execute();
 
-          if ($enregistrement = $stmt->fetch()) {
-              echo '<div class="text-center">';
-              echo $enregistrement->prenom . ' ' . $enregistrement->nom . ' - ';
-              echo $enregistrement->titre . ' (' . $enregistrement->anneeparution . ')<br>';
-              echo '</div>';
-          } else {
-              echo 'Livre non trouvé.';
-          }
-      } else {
-          echo 'Numéro de livre non spécifié.';
-      }
+          if ($enregistrement = $stmt->fetch()) {  // si un enregistrement est trouvé
+            echo '<div class="text-center" style="display: flex; justify-content: center; align-items: center;">';
+            echo '<div>';
+            echo $enregistrement->prenom . ' ' . $enregistrement->nom . ' - '; // afiche le nom de l'auteur 
+            echo $enregistrement->titre . ' (' . $enregistrement->anneeparution . ')';
+            echo '</div>';
+            echo '<form method="post" action="vider_panier.php" style="margin-left: 10px;">';
+            echo '<button type="submit">Annuler</button>';
+            echo '</form>';
+            echo '</div>';
+        }
+        }
       ?>
+      <form method="post" action="valider_panier.php" class="text-center my-4">
+        <button type="submit" class="btn btn-success">Valider Panier</button>
+      </form>
     </div>
     <div class="col-sm-3">
       <img src="biblio.jpg" width="300px" height="350px" alt="biblio">
