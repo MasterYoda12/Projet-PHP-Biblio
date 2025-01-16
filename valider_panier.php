4b2a03ebf752b83bf82a1a61aed1cc93f471fbf9
MasterYoda12 <maxime.ladu33@gmail.com>
2025-01-17 00:09:32 +0100

    changement nom

--- valider_panier.php
+++ valider_panier.php
@@ -6,10 +6,10 @@ require_once('connexion.php');
 
 if (isset($_SESSION['panier'])) { // vérifie si le panier est défini dans la session
     if (isset($_SESSION["mel"])) { // vérifie si l'identifiant est défini dans la session
-        foreach ($_SESSION['panier'] as $key=>$value) { // boucle à travers les livres dans le panier pour les ajouter à la table emprunter
+        foreach ($_SESSION['panier'] as $clé=>$value) { // boucle à travers les livres dans le panier pour les ajouter à la table emprunter
             $stmt = $connexion->prepare("INSERT INTO emprunter (mel, nolivre, dateemprunt) VALUES (:mel, :nolivre, :dateemprunt)");
             $mel = $_SESSION["mel"];
-            $nolivre = $key; // récupère le numéro du livre dans le panier et l'assigne à la variable nolivre
+            $nolivre = $clé; // récupère le numéro du livre dans le panier et l'assigne à la variable nolivre 
             $dateemprunt = date("Y-m-d"); 
             $stmt->bindValue(':mel', $mel, PDO::PARAM_STR);
             $stmt->bindValue(':nolivre', $nolivre, PDO::PARAM_INT); // lie le numéro du livre à la requête SQL
